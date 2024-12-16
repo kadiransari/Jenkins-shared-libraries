542dd2f07c1fcfe910616f72271a95d3143cfec6
Kadir Ansari <125560923+kadiransari@users.noreply.github.com>
2024-12-16 19:37:00 +0530

    Update docker_build.groovy

--- vars/docker_build.groovy
+++ vars/docker_build.groovy
@@ -1,3 +1,3 @@
-def call(string imagename, string imagetag){
-  sh "docker build -t ${imagename}:${imagetag} ."
+def call(String ProjectName, String ImageTag, String DockerHubUser){
+  sh "docker build -t ${DockerHubUser}/${ProjectName}:${ImageTag} ."
 }
